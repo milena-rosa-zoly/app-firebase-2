a891911ff5fdbc92f80d3cab77921679e1952b01
Milena Boselli Rosa <milena.rosa@gmail.com>
2020-08-13 15:25:57 -0300

    first commit

---
+++ packages/core/lib/CoolComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "react-native";
+
+// import { Container } from './styles';
+
+const CoolComponent: React.FC = () => {
+  return <div />;
+};
+
+export default CoolComponent;
